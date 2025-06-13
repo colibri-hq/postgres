86eab463eaec0a2afe6a77512d5dae8c54258525
Moritz Friedrich <moritz@matchory.com>
2025-06-13 23:55:26 +0200

    Fixed missing `--user` flag in pg_cron installation command.
    Fixes #3

--- init.sh
+++ init.sh
@@ -4,6 +4,7 @@
 # be installed in the "postgres" database, overriding the default database using the
 # POSTGRES_DB environment variable would break the installation.
 psql \
+  --command 'create extension "pg_cron" schema pg_catalog;' \
+  --user "${POSTGRES_USER:-postgres}" \
   --dbname postgres \
-  --echo-queries \
-  --command 'create extension "pg_cron" schema pg_catalog;'
+  --echo-queries
